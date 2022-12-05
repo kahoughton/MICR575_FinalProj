0d07840e183417491e8c4faf191cbfc4c9daa393
kahoughton <houghton.katelyn@gmail.com>
2022-12-04 23:34:48 -0500

    details for example data while optimizing workflow

---
+++ Example_Data.R
@@ -0,0 +1,7 @@
+# Example data using 
+# paper: https://www.sciencedirect.com/science/article/pii/S2211124722007434#sec4
+
+# data submitted at NCBI GEO
+# https://www.ncbi.nlm.nih.gov/gds/
+# data project #GSE189033 
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM5693472
