69104c86d0e80bcecc311538a44120c897c874c6
kahoughton <houghton.katelyn@gmail.com>
2022-12-04 23:29:17 -0500

    creating final project repository

---
+++ SnakeMake_Tutorial.R
@@ -0,0 +1,11 @@
+# using snake make 
+
+# 1. enable VPN (pulse secure)
+
+# 2. login to Isaac (via putty, can try mobaxterm if needed later)
+#    khought1@login.isaac.utk.edu  
+#    location: /lustre/isaac/proj/UTK0002/khought1/MICR575/
+
+
+# 3. follow snakemake tutorial at 
+#    https://snakemake.readthedocs.io/en/stable/tutorial/setup.html
