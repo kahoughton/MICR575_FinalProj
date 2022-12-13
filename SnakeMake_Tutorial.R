8e9135287b0c708c9f4c0f9349d13e96e1431181
kahoughton <houghton.katelyn@gmail.com>
2022-12-12 21:15:08 -0500

    updates with completed tutorial notes (still need to upload the actual snakefile from ISAAC)

--- SnakeMake_Tutorial.R
+++ SnakeMake_Tutorial.R
@@ -1,11 +1,38 @@
-# using snake make 
+# using snake make - the tutorial 
 
-# 1. enable VPN (pulse secure)
+# I. enable VPN (pulse secure)
 
-# 2. login to Isaac (via putty, can try mobaxterm if needed later)
+# II. login to Isaac (via putty, can try mobaxterm if needed later)
 #    khought1@login.isaac.utk.edu  
-#    location: /lustre/isaac/proj/UTK0002/khought1/MICR575/
+#    location: /nsf/home/khought1
+#     ( lab location for backup: /lustre/isaac/proj/UTK0002/khought1/MICR575/
 
 
-# 3. follow snakemake tutorial at 
+# III. follow snakemake tutorial at 
 #    https://snakemake.readthedocs.io/en/stable/tutorial/setup.html
+
+#       1.1 Install mambaforge
+#       1.2 make snakemake working directoy
+#       1.3 create environment with req software
+#       1.4 activate environment 
+#       example workflow 
+#       2.0 create blank Snakemake file (just "Snakefile" no *.txt) 
+#       2.1 mapping reads
+#           * there was an error here with dependancies
+#           * had to $ mamba install 'tabulate=0.8.10' when in snake environment
+#           * documentation: https://github.com/snakemake/snakemake/issues/1899
+#       2.2 generalize read mapping rule
+#       2.3 sorting read alignments
+#       2.4 indexing read alignments & vis DAG file
+#           * can vis DAG file in ISAAC on demand
+#       2.5 calling genomic variants
+#       2.6 using custom scripts
+#       2.7 adding a target rule 
+#
+# IV. Snakemake tutorial results 
+#       
+#       1. snakefile
+#       2. DAG file
+#       3. quality file of processing 
+
+
